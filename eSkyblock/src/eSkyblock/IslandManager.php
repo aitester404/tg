98ccdbbd1f8a9f073f0dc2162391a4ff262beb72
aitester404 <42bzl42@gmail.com>
2025-11-22 16:34:58 +0300

    Update IslandManager.php

--- eSkyblock/src/eSkyblock/IslandManager.php
+++ eSkyblock/src/eSkyblock/IslandManager.php
@@ -2,23 +2,28 @@
 
 namespace eSkyblock;
 
-use pocketmine\block\BlockLegacyIds;
 use pocketmine\block\VanillaBlocks;
-use pocketmine\math\Vector3;
 use pocketmine\player\Player;
-use pocketmine\world\World;
 
-class IslandGenerator {
+class IslandManager {
 
-    public static function createIsland(Player $player): void {
+    private Main $plugin;
+
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+
+    public function createIsland(Player $player): void {
         $world = $player->getWorld();
-        $center = $player->getPosition()->floor()->add(0, 10, 0); // oyuncunun üstünde 10 blok yukarıda ada
+        $center = $player->getPosition()->floor()->add(0, 10, 0);
 
-        // 5x5 taş platform
+        // 5x5 platform: üstte çimen, altında toprak
         for($x = -2; $x <= 2; $x++){
             for($z = -2; $z <= 2; $z++){
-                $pos = $center->add($x, 0, $z);
-                $world->setBlock($pos, VanillaBlocks::STONE());
+                $grassPos = $center->add($x, 0, $z);
+                $dirtPos  = $center->add($x, -1, $z);
+                $world->setBlock($grassPos, VanillaBlocks::GRASS());
+                $world->setBlock($dirtPos, VanillaBlocks::DIRT());
             }
         }
 
@@ -26,23 +31,22 @@ class IslandGenerator {
         $chestPos = $center->add(0, 1, 0);
         $world->setBlock($chestPos, VanillaBlocks::CHEST());
 
-        // Sandığa başlangıç itemleri ekle
         $tile = $world->getTile($chestPos);
         if($tile instanceof \pocketmine\block\tile\Chest){
             $inv = $tile->getInventory();
-            $inv->addItem(VanillaBlocks::SAPLING()->asItem()); // ağaç için fidan
-            $inv->addItem(VanillaBlocks::DIRT()->asItem()->setCount(5)); // toprak
+            $inv->addItem(VanillaBlocks::OAK_SAPLING()->asItem());
+            $inv->addItem(VanillaBlocks::DIRT()->asItem()->setCount(5));
             $inv->addItem(VanillaBlocks::ICE()->asItem());
             $inv->addItem(VanillaBlocks::LAVA()->asItem());
         }
 
-        // Ortasına bir toprak + fidan (ağaç için)
+        // Küçük ağaç için toprak + fidan
         $treeBase = $center->add(1, 1, 1);
         $world->setBlock($treeBase, VanillaBlocks::DIRT());
         $world->setBlock($treeBase->add(0, 1, 0), VanillaBlocks::OAK_SAPLING());
 
         // Oyuncuyu adanın üstüne ışınla
         $player->teleport($center->add(0.5, 2, 0.5));
-        $player->sendMessage("§aSkyblock adan hazır!");
+        $player->sendMessage("§aSkyblock adan oluşturuldu!");
     }
 }
