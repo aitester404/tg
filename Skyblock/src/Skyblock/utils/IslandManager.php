7ababd2dfe0c75741ad4a45a0c640ee026cd3335
aitester404 <42bzl42@gmail.com>
2025-11-22 04:07:08 +0300

    Update IslandManager.php

--- Skyblock/src/Skyblock/utils/IslandManager.php
+++ Skyblock/src/Skyblock/utils/IslandManager.php
@@ -6,17 +6,19 @@ use pocketmine\player\Player;
 use pocketmine\Server;
 use pocketmine\world\WorldCreationOptions;
 use pocketmine\world\Position;
+use pocketmine\world\generator\VoidGenerator;
 
 class IslandManager {
 
     public static function createIsland(Player $player) : void {
         $worldName = "island_" . strtolower($player->getName());
-
         $wm = Server::getInstance()->getWorldManager();
 
-        // Dünya yoksa oluştur
+        // Dünya yoksa void olarak oluştur
         if(!$wm->isWorldGenerated($worldName)){
-            $wm->generateWorld($worldName, new WorldCreationOptions());
+            $options = new WorldCreationOptions();
+            $options->setGeneratorClass(VoidGenerator::class);
+            $wm->generateWorld($worldName, $options);
         }
 
         // Dünyayı yükle
@@ -34,11 +36,13 @@ class IslandManager {
         // Ada koordinatı
         $x = 0; $y = 100; $z = 0;
 
-        // EasyEdit komutunu oyuncu üzerinden çalıştır
-        Server::getInstance()->dispatchCommand($player, "easyedit paste myisland $x $y $z $worldName");
+        // EasyEdit schematic dosyasını console üzerinden yapıştır
+        $console = Server::getInstance()->getCommandSender();
+        Server::getInstance()->dispatchCommand($console, "easyedit paste myisland $x $y $z $worldName");
 
         // Oyuncuyu ışınla
         $player->teleport(new Position($x, $y, $z, $world));
         $player->sendMessage("§aKendi Skyblock adan hazır!");
     }
 }
+
