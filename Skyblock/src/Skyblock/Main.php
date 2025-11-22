925db14991d978c46cf45f4c515d2271c01a159f
aitester404 <42bzl42@gmail.com>
2025-11-22 04:01:16 +0300

    Update Main.php

--- Skyblock/src/Skyblock/Main.php
+++ Skyblock/src/Skyblock/Main.php
@@ -3,10 +3,12 @@
 namespace Skyblock;
 
 use pocketmine\plugin\PluginBase;
+use Skyblock\commands\AdaCommand;
 
 class Main extends PluginBase {
 
     public function onEnable() : void {
         $this->getLogger()->info("Skyblock plugin aktif!");
+        $this->getServer()->getCommandMap()->register("ada", new AdaCommand($this));
     }
 }
