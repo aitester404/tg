138a5e97e788e5d8c138592307d029c095c288ff
aitester404 <42bzl42@gmail.com>
2025-11-22 04:13:00 +0300

    Update IslandManager.php

--- Skyblock/src/Skyblock/utils/IslandManager.php
+++ Skyblock/src/Skyblock/utils/IslandManager.php
@@ -4,9 +4,7 @@ namespace Skyblock\utils;
 
 use pocketmine\player\Player;
 use pocketmine\Server;
-use pocketmine\world\WorldCreationOptions;
 use pocketmine\world\Position;
-use pocketmine\world\generator\VoidGenerator;
 use pocketmine\console\ConsoleCommandSender;
 
 class IslandManager {
@@ -15,11 +13,10 @@ class IslandManager {
         $worldName = "island_" . strtolower($player->getName());
         $wm = Server::getInstance()->getWorldManager();
 
-        // Dünya yoksa void olarak oluştur
+        // Dünya yoksa EasyEdit ile void olarak oluştur (komut)
         if(!$wm->isWorldGenerated($worldName)){
-            $options = new WorldCreationOptions();
-            $options->setGeneratorClass(VoidGenerator::class);
-            $wm->generateWorld($worldName, $options);
+            $console = new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage());
+            Server::getInstance()->dispatchCommand($console, "easyedit createworld $worldName void");
         }
 
         // Dünyayı yükle
@@ -38,7 +35,6 @@ class IslandManager {
         $x = 0; $y = 100; $z = 0;
 
         // EasyEdit schematic dosyasını console üzerinden yapıştır
-        // Dosya yolu plugin_data/EasyEdit/schematics/myisland.schem olmalı
         $console = new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage());
         Server::getInstance()->dispatchCommand($console, "easyedit paste myisland $x $y $z $worldName");
 
