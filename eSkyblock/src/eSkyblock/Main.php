6b7dc9c5ee0bbd5cb7ddc9edbd7f4f71e2e32305
aitester404 <42bzl42@gmail.com>
2025-11-22 16:36:36 +0300

    Update Main.php

--- eSkyblock/src/eSkyblock/Main.php
+++ eSkyblock/src/eSkyblock/Main.php
@@ -5,41 +5,29 @@ namespace eSkyblock;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use eSkyblock\Menu;
-use eSkyblock\IslandManager;
-use eSkyblock\LevelManager;
+use pocketmine\player\Player;
 
 class Main extends PluginBase {
 
-    private static Main $instance;
     private IslandManager $islandManager;
-    private LevelManager $levelManager;
 
     public function onEnable(): void {
-        self::$instance = $this;
-        @mkdir($this->getDataFolder() . "players/");
-        $this->saveResource("config.yml");
-        $this->saveResource("rank.yml");
-
         $this->islandManager = new IslandManager($this);
-        $this->levelManager = new LevelManager($this);
-    }
-
-    public static function getInstance(): Main {
-        return self::$instance;
+        $this->getLogger()->info("§aSkyblock plugin aktif!");
     }
 
     public function getIslandManager(): IslandManager {
         return $this->islandManager;
     }
 
-    public function getLevelManager(): LevelManager {
-        return $this->levelManager;
-    }
-
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if($command->getName() === "ada"){
-            (new Menu($this))->openMenu($sender);
+            if($sender instanceof Player){
+                $menu = new Menu($this); // Menü sınıfını oluştur
+                $sender->sendForm($menu); // Menü açılır
+            } else {
+                $sender->sendMessage("§cBu komut sadece oyunda kullanılabilir!");
+            }
             return true;
         }
         return false;
